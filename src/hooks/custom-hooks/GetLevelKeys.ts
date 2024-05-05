750f57da88aad602bd6bdc7f269ead83a123e355
KimKyuHoi <k546kh@gmail.com>
2024-05-06 05:43:07 +0900

    Feat: Slider 구현

---
+++ src/hooks/custom-hooks/GetLevelKeys.ts
@@ -0,0 +1,20 @@
+export interface LevelKeysProps {
+  key?: string;
+  children?: LevelKeysProps[];
+}
+
+export const GetLevelKeys = (items1: LevelKeysProps[]) => {
+  const key: Record<string, number> = {};
+  const func = (items2: LevelKeysProps[], level = 1) => {
+    items2.forEach((item) => {
+      if (item.key) {
+        key[item.key] = level;
+      }
+      if (item.children) {
+        func(item.children, level + 1);
+      }
+    });
+  };
+  func(items1);
+  return key;
+};
