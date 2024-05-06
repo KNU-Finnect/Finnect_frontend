75d6cbe2548215e22f716c6908baf6739924e738
KimKyuHoi <k546kh@gmail.com>
2024-05-07 05:32:20 +0900

    Feat: GlobalLayout, Router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import ErrorPage from './pages/ErrorPage';
 import RootPage from './pages/RootPage';
-import WelcomePage from '@finnect/pages/welcome/WelcomePage';
+import RecordsRootPage from '@finnect/pages/RecordsRootPage';
+import CompaniesPage from '@finnect/pages/companies/CompaniesPage';
+import DealsPage from '@finnect/pages/deals/DealsPage';
+import PeoplePage from '@finnect/pages/people/PeoplePage';
+import ViewPage from '@finnect/pages/views/ViewPage';
+import WorkSpacePage from '@finnect/pages/workspace/WorkSpacePage';
 import { GlobalStyle } from '@finnect/styles/GlobalStyle';
 
 const router = createBrowserRouter([
@@ -11,7 +16,38 @@ const router = createBrowserRouter([
     element: <RootPage />,
     id: 'root',
     errorElement: <ErrorPage />,
-    children: [{ index: true, element: <WelcomePage /> }],
+    children: [
+      { index: true, element: <WorkSpacePage /> },
+      {
+        path: 'records',
+        element: <RecordsRootPage />,
+        children: [
+          {
+            index: true,
+            path: 'companies',
+            element: <CompaniesPage />,
+          },
+          {
+            path: 'people',
+            element: <PeoplePage />,
+          },
+          {
+            path: 'deals',
+            element: <DealsPage />,
+          },
+        ],
+      },
+      {
+        path: 'views',
+        children: [
+          {
+            index: true,
+            path: ':viewName',
+            element: <ViewPage />,
+          },
+        ],
+      },
+    ],
   },
 ]);
 
