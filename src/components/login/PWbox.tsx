e8b227f0a12dde6d859a6ffb19d2785922037869
Dongpil Jo <ehdvlf001@knu.ac.kr>
2024-05-25 18:10:21 +0900

    feat: password check 구현

--- src/components/login/PWbox.tsx
+++ src/components/login/PWbox.tsx
@@ -1,22 +1,30 @@
+import React from 'react';
 import styled from 'styled-components';
 import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
-import { Input,Typography } from 'antd';
+import { Input, Typography } from 'antd';
 
-const PWbox = () => {
-    return (
-        <PWboxWrapper>
-            <Typography.Title level={5}>Password</Typography.Title>
-            <Input.Password
-                placeholder="input password"
-                iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
-            />
-        </PWboxWrapper>
-    );
+interface PWboxProps {
+  password: string;
+  onPasswordChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
+
+const PWbox: React.FC<PWboxProps> = ({ password, onPasswordChange }) => {
+  return (
+    <PWboxWrapper>
+      <Typography.Title level={5}>Password</Typography.Title>
+      <Input.Password
+        value={password}
+        onChange={onPasswordChange}
+        placeholder="input password"
+        iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+      />
+    </PWboxWrapper>
+  );
+};
 
 export default PWbox;
 
 const PWboxWrapper = styled.div`
-    margin-bottom: 0;
-    position: relative;
+  margin-bottom: 0;
+  position: relative;
 `;
