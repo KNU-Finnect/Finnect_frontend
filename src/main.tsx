e076f4f4b6e189c337ab09eb5791bc444740c8b4
KimKyuHoi <k546kh@gmail.com>
2024-05-26 17:26:14 +0900

    feat: msw 환경 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,28 @@ import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 
 import App from '@finnect/App';
+import { worker } from '@finnect/mocks/Browser';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-    <RecoilRoot>
-      <ReactQueryDevtools initialIsOpen={true} />
-      <App />
-    </RecoilRoot>
-  </QueryClientProvider>
-);
+const enableMocking = async () => {
+  if (import.meta.env.NODE_ENV === 'development') {
+    return;
+  }
+
+  // console.log('start!');
+  return worker.start();
+};
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <ReactQueryDevtools initialIsOpen={true} />
+        <App />
+      </RecoilRoot>
+    </QueryClientProvider>
+  );
+});
