290ddfadbb3d9e9de2d44abf71debf074834bc88
KimKyuHoi <k546kh@gmail.com>
2024-05-26 18:47:13 +0900

    feat: tanstack query 작성 GET WORKSPACES

---
+++ src/hooks/queries/workspace/useGetWorkSpaceQuery.ts
@@ -0,0 +1,14 @@
+import { useGetWorkspace } from '@finnect/apis/workspace/useGetWorkSpace';
+
+import { useGetWorkspace } from '@finnect/apis/workspace/useGetWorkSpace';
+
+import { useQuery } from '@tanstack/react-query';
+
+export const useGetWorkSpaceQuery = () => {
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['getWorkspace'],
+    queryFn: useGetWorkspace,
+  });
+
+  return { data, isPending, isError, error };
+};
