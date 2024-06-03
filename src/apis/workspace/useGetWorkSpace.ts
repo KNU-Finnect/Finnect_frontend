6083afe96a89f823f956b8b5175f2e78700fb3c1
KimKyuHoi <k546kh@gmail.com>
2024-06-03 22:46:42 +0900

    feat: workspace 받아오기 및 만들기 구현

--- src/apis/workspace/useGetWorkSpace.ts
+++ src/apis/workspace/useGetWorkSpace.ts
@@ -3,6 +3,7 @@ import { axiosClient } from '../AxiosClient';
 export const useGetWorkspace = async () => {
   try {
     const response = await axiosClient.get(`/workspaces`);
+
     return response.data;
   } catch (error) {
     console.error(error);
