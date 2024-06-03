46ffd3baf22953906aafe1bbbefb50bd46b2bb38
KimKyuHoi <k546kh@gmail.com>
2024-06-03 23:29:42 +0900

    feat: people 데이터 받아오기 및 post 구현

--- src/apis/people/useDeletePeople.ts
+++ src/apis/people/useDeletePeople.ts
@@ -1,10 +1,8 @@
 import { axiosClient } from '@finnect/apis/AxiosClient';
 
-import { BASE_URI } from '@finnect/constants/URI';
-
 export const useDeletePeople = async () => {
   try {
-    const response = await axiosClient.delete(`${BASE_URI}/workspaces/people`);
+    const response = await axiosClient.delete(`/workspaces/people`);
     return response.data;
   } catch (error) {
     console.error(error);
