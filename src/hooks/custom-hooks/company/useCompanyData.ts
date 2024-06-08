e6d4adfd9a766e3ab294d502d8adb7514bcb2e3e
KimKyuHoi <k546kh@gmail.com>
2024-06-08 23:08:24 +0900

    remove: 코드 제거

--- src/hooks/custom-hooks/company/useCompanyData.ts
+++ src/hooks/custom-hooks/company/useCompanyData.ts
@@ -1,4 +1,3 @@
-import { ColDef } from 'ag-grid-community';
 import { useRecoilState } from 'recoil';
 
 import { columnDefsState } from '@finnect/atoms/company/useCompany';
@@ -6,40 +5,13 @@ import { columnDefsState } from '@finnect/atoms/company/useCompany';
 import { useGetWC } from '@finnect/hooks/queries/company/useGetWC';
 import { usePostWC } from '@finnect/hooks/queries/company/usePostWC';
 
-import { CompanyColumnInterface } from '@finnect/interface/CompanyInterface';
 import { useGetCV } from '@finnect/hooks/queries/company/useGetCV';
 
 export const useCompanyData = () => {
-  const [columnDefs, setColumnDefs] = useRecoilState(columnDefsState);
+  const [columnDefs] = useRecoilState(columnDefsState);
   const { refetch: CompanyList } = useGetWC();
   const { refetch: CV } = useGetCV();
   const { mutate } = usePostWC();
-
-  const addColumn = ({ name, type }: CompanyColumnInterface) => {
-    let newColumnDef: ColDef;
-
-    if (type === 'text') {
-      newColumnDef = {
-        headerName: name,
-        field: name,
-        editable: true,
-      };
-    } else if (type === 'number') {
-      newColumnDef = {
-        headerName: name,
-        field: name,
-        editable: true,
-      };
-    } else if (type === 'date') {
-      newColumnDef = {
-        headerName: name,
-        field: name,
-        editable: true,
-      };
-    }
-
-    setColumnDefs((prevDefs) => [...prevDefs, newColumnDef]);
-  };
 
   const addCompany = async (name: string, domain: string) => {
     mutate(
@@ -55,7 +27,6 @@ export const useCompanyData = () => {
 
   return {
     columnDefs,
-    addColumn,
     addCompany,
   };
 };
