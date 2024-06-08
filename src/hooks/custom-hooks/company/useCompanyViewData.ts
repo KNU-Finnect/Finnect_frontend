e164620d9d33ee74b3cc4f6db7f9c6614316d4b6
KimKyuHoi <k546kh@gmail.com>
2024-06-08 21:44:13 +0900

    remove: 필요없는 함수 제거

--- src/hooks/custom-hooks/company/useCompanyViewData.ts
+++ src/hooks/custom-hooks/company/useCompanyViewData.ts
@@ -8,7 +8,6 @@ import {
 import { useGetCV } from '@finnect/hooks/queries/company/useGetCV';
 
 import { RowData } from '@finnect/interface/CompanyInterface';
-import { usePWcpCellQ } from '@finnect/hooks/queries/company/usePWcpCellQ';
 
 import CustomCellEditor from '@finnect/pages/companies/components/CustomCellEditor';
 import CustomCategoryEditor from '@finnect/pages/companies/components/CustomCategoryEditor';
@@ -17,7 +16,6 @@ export const useCompanyViewData = () => {
   const [rowData, setRowData] = useRecoilState(rowDataState);
   const [columnDefs, setColumnDefs] = useRecoilState(columnDefsState);
   const { data, isPending, isError, error } = useGetCV();
-  const { mutate } = usePWcpCellQ();
 
   useEffect(() => {
     if (
@@ -69,31 +67,11 @@ export const useCompanyViewData = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [data]);
 
-  const handleCellValueChanged = async (params: any) => {
-    console.log('params', params);
-    const { data, colDef, newValue } = params;
-    const column = data.result.viewColumns.find(
-      (col: any) => col.columnName === colDef.field
-    );
-
-    if (column) {
-      mutate({
-        columnId: column.columnId,
-        rowId: data.rowId,
-        value: newValue,
-        companyId: data.companyId,
-      });
-    }
-
-    console.log('params', params);
-  };
-
   return {
     rowData,
     columnDefs,
     isPending,
     isError,
     error,
-    handleCellValueChanged,
   };
 };
